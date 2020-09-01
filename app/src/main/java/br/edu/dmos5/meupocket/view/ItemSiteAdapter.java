7e4b885962a9e803f505625289c3d87bb1af8a7f
IagoRodrigues <rodrigues-iago@hotmail.com>
2020-09-01 16:11:54 -0300

    Versão 2
    Funcional!

---
+++ app/src/main/java/br/edu/dmos5/meupocket/view/ItemSiteAdapter.java
@@ -0,0 +1,121 @@
+package br.edu.dmos5.meupocket.view;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import java.util.List;
+
+import br.edu.dmos5.meupocket.R;
+import br.edu.dmos5.meupocket.model.Site;
+
+/**
+ * A classe ItemSiteAdapter é responsável por fazer a ligação da fonte de dados
+ * com a ListView que irá apresentá-los. É nessa implementação que devemos
+public class ItemSiteAdapter {* especificar o comportamento dos elementos da Lista
+ */
+public class ItemSiteAdapter extends ArrayAdapter {
+
+    //Objeto responsável por "inflar" (criar) um elemento
+    //na tela
+    private LayoutInflater inflater;
+
+    /**
+     * Método construtor responsável por instânciar o adapter. Vale observar que
+     * a classe é uma extensão de ArrayAdapter, por isso é necessário utilizar
+     * um dos construtores da classe pai.
+     * @param context
+     * @param siteList
+     */
+    public ItemSiteAdapter(Context context, List<Site> siteList) {
+        super(context, R.layout.item_lista_pocket, siteList);
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    /**
+     * O método getView() é invocado quando os ListView é apresentado. É importante observar que
+     * o ListView possui os objetos que estão na tela, os demais objetos "escondidos" não fazem
+     * parte o ListView, por isso, o método getView é invocado sempre que a lista é rolada,
+     * trocando os elementos que estão visíveis.
+     * @param position
+     * @param convertView
+     * @param parent
+     * @return
+     */
+    @NonNull
+    @Override
+    public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent)
+    {
+        final ViewHolder holder;
+        if (convertView == null) {
+            /*
+                O objeto convertView será nulo quando ainda não foi instânciado, ou seja, em nosso
+                exemplo é invocado quando o site ainda não está na ListView, dessa forma, é preciso
+                instância o objeto (inflar um novo item_lista_pocket) e obter as referências de cada
+                elemento de layout.
+            */
+            convertView = inflater.inflate(R.layout.item_lista_pocket, null);
+            holder = new ViewHolder();
+            holder.tituloTextView = convertView.findViewById(R.id.text_titulo);
+            holder.enderecoTextView = convertView.findViewById(R.id.text_endereco);
+            holder.favoritoImageView = convertView.findViewById(R.id.image_favorito);
+
+            /*
+                Uma View possui um atributo do tipo Object denominado tag no qual podemos guardar
+                qualquer objeto instânciado. Em nosso exemplo, como as referências dos elementos
+                visuais da lista estão em um holder, vamos armazenar esse objeto na tag para que
+                posteriormente ele seja recuparado.
+            */
+            convertView.setTag(holder);
+        } else {
+
+            /*
+                Esse else indicada que o contentView já existe, assim precisamos ajustar nosso objeto
+                holder para o holder que foi armazenado na tag da View, sendo necessário apenas
+                recuperar a tag.
+            */
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        /*
+            É importante lembrar que estamos implementando uma classe que estende um ArrayAdapter,
+            ou seja, possui todas as características da classe pai.
+            O método getView() recebe como argumento a posição do elemento da fonte de dados que
+            precisa ser apresentado, lembrando que a fonte de dados foi inserida no objeto pai pelo
+            método construtor.
+            Aqui vamos recuperar o o objeto (Site) que está na fonte de dados utilizando o método
+            getItem() do ArrayAdapter. Com o objeto recuperado vamos inserir os dados dele nos
+            elementos de layout que possuímos e estão com a referência dentro do holder. Atenção
+            para a imagem, que recuperamos uma imagem de acordo com a preferência do usuário
+            indicada no objeto Site.
+        */
+        Site obj = (Site) getItem(position);
+        holder.tituloTextView.setText(obj.getTitulo());
+        holder.enderecoTextView.setText(obj.getEndereco());
+        if (obj.isFavorito())
+            holder.favoritoImageView.setImageResource(R.drawable.ic_favorito);
+        else
+            holder.favoritoImageView.setImageResource(R.drawable.ic_nao_favorito);
+        /*
+            Depois de tudo pronto basta devolver o contentView para que seja apresentado pelo
+            ListView
+        */
+        return convertView;
+    }
+
+    /*
+        A classe ViewHolder é uma inner class, ou seja, uma classe interna. Ela consegue "enchergar"
+        tudo da classe ItemSiteAdapter. Vamos utilizar essa classe para armazenar as referências dos
+        elementos de layout que estão no layout item_lista_pocket.
+    */
+    private static class ViewHolder {
+        public TextView tituloTextView;
+        public TextView enderecoTextView;
+        public ImageView favoritoImageView;
+    }
+}
--- app/src/main/java/br/edu/dmos5/meupocket/view/SitesActivity.java
+++ app/src/main/java/br/edu/dmos5/meupocket/view/SitesActivity.java
@@ -30,6 +30,7 @@ public class SitesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sites);
+
         //Recupera a referência do elemento no layout
         sitesListView = findViewById(R.id.list_sites);
 
@@ -37,11 +38,23 @@ public class SitesActivity extends AppCompatActivity {
         siteList = SiteDao.recuperateAll();
 
         //Instancia do adapter, aqui configura-se como os dados serão apresentados e também
-        //qual a fonte de dados será utilizada.
-        siteArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, siteList);
+        //qual a fonte de dados será utilizada, agora com NOSSO adapter.
+        //siteArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, siteList);
+        siteArrayAdapter = new ItemSiteAdapter(this, siteList);
 
         //Com o adapter pronto, vinculamos ele ao nosso ListView. Após esse comando o
         //ListView já consegue apresentar os dados na tela.
         sitesListView.setAdapter(siteArrayAdapter);
+
+        //Insere-se um listener para os itens da ListView. O método onItemClick() possui um
+        //argumento que indica qual o elemento (posição) foi clicado, assim, basta recuperar esse
+        //elemento de nossa lista e temos o objeto.
+        sitesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getApplicationContext(), siteList.get(i).getTitulo() + "\n" +
+                        siteList.get(i).getEndereco(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
